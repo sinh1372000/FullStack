655f7990b4b53613acbae849257277528ec6e2b4
NguyenSinh <sinhnd2000@gmail.com>
2022-04-14 08:43:52 +0700

    #55 Hoàn thiện menu phân quyền

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -121,7 +121,9 @@ class HomeHeader extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
+        language: state.app.language,
     }
 }
 
